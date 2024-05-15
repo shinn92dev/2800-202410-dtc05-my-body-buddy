c2d1c040df8f6e4c72262b6fda8bde28d19508dc
Anthony Shin <shinn92dev@gmail.com>
2024-05-14 21:11:57 -0700

    :sparkles:feat (home): implement routing logic to sign up page

---
+++ app/(authentication)/signup/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: "Sign Up",
+};
+
+export default function SignUp() {
+    return (
+        <div>
+            <h1>This is My Body Buddy SignUp page</h1>
+        </div>
+    );
+}
