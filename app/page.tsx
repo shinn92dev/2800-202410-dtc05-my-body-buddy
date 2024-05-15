21cc01b510bbeb04f61af24d6b3bed33669aab64
Anthony Shin <shinn92dev@gmail.com>
2024-05-14 21:03:13 -0700

    :tada:init: initialize Next.js App

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: "HOME",
+};
+
+export default function Home() {
+    return (
+        <div>
+            <h1>This is My Body Buddy Home page</h1>
+        </div>
+    );
+}
