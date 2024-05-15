29ae600a5a63baaf8a82ddd6971e002709cd4c72
Anthony Shin <shinn92dev@gmail.com>
2024-05-14 21:13:04 -0700

    :sparkles:feat (home): implement routing logic to log in page

---
+++ app/(authentication)/login/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: "Log In",
+};
+
+export default function LogIn() {
+    return (
+        <div>
+            <h1>This is My Body Buddy Log In page</h1>
+        </div>
+    );
+}
