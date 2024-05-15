124f3d683f8977b1396d16a8e4094c8d2f401922
Anthony Shin <shinn92dev@gmail.com>
2024-05-14 21:16:19 -0700

    :sparkles:feat (summary): implement routing logic to diet summary page

---
+++ app/(summary)/summary/diet/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: "Diet Summary",
+};
+
+export default function DietSummary() {
+    return (
+        <div>
+            <h1>This is My Body Buddy Diet Summary page</h1>
+        </div>
+    );
+}
