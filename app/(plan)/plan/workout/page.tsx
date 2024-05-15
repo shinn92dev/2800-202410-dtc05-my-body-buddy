6eaffb42fe10b4a29873932cf8e6c227ec3f7d10
Anthony Shin <shinn92dev@gmail.com>
2024-05-14 21:20:02 -0700

    :sparkles:feat (plan): implement routing logic to workout plan page

---
+++ app/(plan)/plan/workout/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: "Diet Plan",
+};
+
+export default function DietPlan() {
+    return (
+        <div>
+            <h1>This is My Body Buddy Diet Plan page</h1>
+        </div>
+    );
+}
