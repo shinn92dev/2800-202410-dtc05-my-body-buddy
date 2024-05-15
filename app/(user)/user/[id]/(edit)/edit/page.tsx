0caf779d92477b27beb743cb91bbcff13f9781e3
Anthony Shin <shinn92dev@gmail.com>
2024-05-14 21:34:38 -0700

    :tada:feat (user/edit): implement routing logic to user basic information edit page using id param

---
+++ app/(user)/user/[id]/(edit)/edit/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: "User",
+};
+
+export default function UserEdit({ params }: { params: { id: string } }) {
+    const { id } = params;
+    return (
+        <div>
+            <h1>This is My Body Buddy User ({id}) Edit page</h1>
+        </div>
+    );
+}
