e1ef2e84033cf33b06f56604bcc330ed511d0253
Anthony Shin <shinn92dev@gmail.com>
2024-05-14 21:16:56 -0700

    :sparkles:feat (summary): implement routing logic to workout summary page

---
+++ app/(summary)/summary/workout/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: "Workout Summary",
+};
+
+export default function WorkoutSummary() {
+    return (
+        <div>
+            <h1>This is My Body Buddy Workout Summary page</h1>
+        </div>
+    );
+}
