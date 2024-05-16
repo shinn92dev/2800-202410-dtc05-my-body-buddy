0bef58b59ad930edc5768e1daf7e39c3f6cd6a4a
Anthony Shin <shinn92dev@gmail.com>
2024-05-16 02:42:03 -0700

    :recycle:refactor (icons): separate Google icon, Signin icon, and Login icon as components and replace current svg tag to corresponding component

--- app/(authentication)/signup/page.tsx
+++ app/(authentication)/signup/page.tsx
@@ -1,4 +1,6 @@
 import InputBox from "@/components/global/InputBox";
+import GoogleIcon from "@/components/global/icons/GoogleIcon";
+import SignUpAndInIcon from "@/components/global/icons/SignUpAndInIcon";
 import Image from "next/image";
 
 export const metadata = {
@@ -72,18 +74,7 @@ export default function SignUp() {
                                         </a>
                                     </p>
                                     <button className="mt-2 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
-                                        <svg
-                                            className="w-6 h-6 -ml-2"
-                                            fill="none"
-                                            stroke="currentColor"
-                                            stroke-width="2"
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                        >
-                                            <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
-                                            <circle cx="8.5" cy="7" r="4" />
-                                            <path d="M20 8v6M23 11h-6" />
-                                        </svg>
+                                        <SignUpAndInIcon width={6} />
                                         <span className="ml-3">Sign Up</span>
                                     </button>
                                 </form>
@@ -97,27 +88,7 @@ export default function SignUp() {
                                 <div className="flex flex-col items-center">
                                     <button className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 hover:bg-indigo-200 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none focus:shadow-sm focus:shadow-outline">
                                         <div className="bg-white p-2 rounded-full">
-                                            <svg
-                                                className="w-4"
-                                                viewBox="0 0 533.5 544.3"
-                                            >
-                                                <path
-                                                    d="M533.5 278.4c0-18.5-1.5-37.1-4.7-55.3H272.1v104.8h147c-6.1 33.8-25.7 63.7-54.4 82.7v68h87.7c51.5-47.4 81.1-117.4 81.1-200.2z"
-                                                    fill="#4285f4"
-                                                />
-                                                <path
-                                                    d="M272.1 544.3c73.4 0 135.3-24.1 180.4-65.7l-87.7-68c-24.4 16.6-55.9 26-92.6 26-71 0-131.2-47.9-152.8-112.3H28.9v70.1c46.2 91.9 140.3 149.9 243.2 149.9z"
-                                                    fill="#34a853"
-                                                />
-                                                <path
-                                                    d="M119.3 324.3c-11.4-33.8-11.4-70.4 0-104.2V150H28.9c-38.6 76.9-38.6 167.5 0 244.4l90.4-70.1z"
-                                                    fill="#fbbc04"
-                                                />
-                                                <path
-                                                    d="M272.1 107.7c38.8-.6 76.3 14 104.4 40.8l77.7-77.7C405 24.6 339.7-.8 272.1 0 169.2 0 75.1 58 28.9 150l90.4 70.1c21.5-64.5 81.8-112.4 152.8-112.4z"
-                                                    fill="#ea4335"
-                                                />
-                                            </svg>
+                                            <GoogleIcon width={4} />
                                         </div>
                                         <span className="ml-4">
                                             Sign Up with Google
