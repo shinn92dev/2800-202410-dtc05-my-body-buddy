7d88f59d1731d59ce9d5d6b4060d890a260ff09b
Momona-Wada <momona.9410@gmail.com>
2024-05-17 15:03:08 +0900

    🎨style (encourage.tsx): change background colour

--- app/(home)/encourage.tsx
+++ app/(home)/encourage.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Encourage = () => {
     return (
-        <div className="flex flex-col items-center justify-center bg-orange-100">
-            <div className="text-2xl font-bold text-center">
+        <div className="flex flex-col items-center justify-center bg-beige">
+            <div className="text-xl font-bold text-center">
                 When do you want to start?
                 <br />
                 NOW!
