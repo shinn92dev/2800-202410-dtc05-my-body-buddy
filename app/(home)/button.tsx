9ff86ae72051786f901edfcd3c6764d36ec8f131
Momona-Wada <momona.9410@gmail.com>
2024-05-17 16:35:18 +0900

    🎨style: remove unnecessary classes

--- app/(home)/button.tsx
+++ app/(home)/button.tsx
@@ -4,12 +4,12 @@ const Button = () => {
     return (
         <div className="flex justify-center space-x-4 p-8">
             <a href="/signup">
-                <button className="bg-logo-pumpkin text-white font-bold py-2 px-4 rounded transition duration-300">
+                <button className="bg-logo-pumpkin text-white font-bold py-2 px-4 rounded">
                     Sign Up
                 </button>
             </a>
             <a href="/login">
-                <button className="bg-orange text-white font-bold py-2 px-4 rounded transition duration-300">
+                <button className="bg-orange text-white font-bold py-2 px-4 rounded">
                     Log In
                 </button>
             </a>
