dfba9e3ac1f7b5c42abd80c09783129bbcb91867
Momona-Wada <momona.9410@gmail.com>
2024-05-17 14:11:06 +0900

    ✨feat: create logo.tsx

---
+++ app/(home)/logo.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Logo = () => {
+    return (
+        <div className="flex flex-col items-center justify-center py-10">
+            <img src="/my_body_buddy_logo.png" alt="My Body Buddy Logo" 
+            className="w-32 h-32 rounded"/>
+        </div>
+    );
+};
+
+export default Logo;
