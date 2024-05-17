3cc74f1cbff26213658a3711c61aa21d81f4242b
Momona-Wada <momona.9410@gmail.com>
2024-05-17 14:12:41 +0900

    ✨feat: add logo component into page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import { connectMongoDB } from "@/config/db";
 import { GetCurrentUsersFromMongoDB } from "../_helper/users";
 import Introduction from "./introduction";
+import Logo from "./logo";
 
 connectMongoDB();
 
@@ -11,12 +12,15 @@ export const metadata = {
 export default async function Home() {
     await GetCurrentUsersFromMongoDB();
     return (
-        <div className="bg-cover bg-center bg-no-repeat min-w-full flex flex-col items-center justify-center" style={{ 
-            backgroundImage: "url('/healthy_people_temporal_image.png')",
-            backgroundSize: "cover", // fit the image to the container
-            color: "black",
-        }}>
-            <Introduction />
+        <div>
+            <div className="bg-cover bg-center bg-no-repeat min-w-full flex flex-col items-center justify-center" style={{
+                backgroundImage: "url('/healthy_people_temporal_image.png')",
+                backgroundSize: "cover", // fit the image to the container
+                color: "black",
+            }}>
+                <Introduction />
+            </div>
+            <Logo />
         </div>
     );
 }
