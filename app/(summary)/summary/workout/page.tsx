65ec5319ff6e2c45c56717f2417561699b154054
Momona-Wada <momona.9410@gmail.com>
2024-05-22 09:41:26 +0900

    ✨feat: initial commit on summary branch

--- app/(summary)/summary/workout/page.tsx
+++ app/(summary)/summary/workout/page.tsx
@@ -8,6 +8,7 @@ export default function WorkoutSummary() {
             <h1 className="text-2xl font-bold p-2 m-2">
                 This is My Body Buddy Workout Summary page
             </h1>
+            <div>Hello</div>
         </div>
     );
 }
