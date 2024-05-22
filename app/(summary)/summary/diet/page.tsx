019beca58c211f849044939c35bbeabd9aa5edac
Momona-Wada <momona.9410@gmail.com>
2024-05-23 05:14:08 +0900

    🔨refactor: change label for bar graphs

--- app/(summary)/summary/diet/page.tsx
+++ app/(summary)/summary/diet/page.tsx
@@ -10,19 +10,23 @@ export const metadata = {
 export default function DietSummary() {
     const score = 80; // Replace with a logic to get the score
     const percent = 80; // Replace with a logic to get the percentage
-    const monthlyTotalCalories = 5000; // Replace with a logic to get the monthly total calories
-    const weeklyTotalCalories = 1200; // Replace with a logic to get the weekly total calories
-    const dailyTotalCalories = 200; // Replace with a logic to get the daily total calories
-    const maxCalories = 10000;
+    const dailyTotalCalories = 4000; // Replace with a logic to get the monthly total calories
+    const breakfastTotalCalories = 1000; // Replace with a logic to get the weekly total calories
+    const lunchTotalCalories = 1000; // Replace with a logic to get the lunch total calories
+    const dinnerTotalCalories = 1500; // Replace with a logic to get the dinner total calories
+    const snackTotalCalories = 500; // Replace with a logic to get the snack total calories
+    const maxCalories = 5000;
     return (
         <div>
             <TopCalendar/>
             <div className="text-center">
                 <ScoreCircleBarWrapper score={score} percent={percent} />
                 <div className='text-xl font-semibold p-4'>Calorie Took</div>
-                <BarGraph label="Monthly total" value={monthlyTotalCalories} maxValue={maxCalories}/>
-                <BarGraph label="Weekly total" value={weeklyTotalCalories} maxValue={maxCalories}/>
                 <BarGraph label="Daily total" value={dailyTotalCalories} maxValue={maxCalories}/>
+                <BarGraph label="Breakfast total" value={breakfastTotalCalories} maxValue={maxCalories}/>
+                <BarGraph label="Lunch total" value={lunchTotalCalories} maxValue={maxCalories}/>
+                <BarGraph label="Dinner total" value={dinnerTotalCalories} maxValue={maxCalories}/>
+                <BarGraph label="Snack total" value={snackTotalCalories} maxValue={maxCalories}/>
             </div>
         </div>
     );
