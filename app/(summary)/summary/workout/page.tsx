2dd983d1622d853ce6b4e5e0a497042f472de635
Momona-Wada <momona.9410@gmail.com>
2024-05-23 05:02:06 +0900

    feat: create summary page for workout

--- app/(summary)/summary/workout/page.tsx
+++ app/(summary)/summary/workout/page.tsx
@@ -1,14 +1,32 @@
+import React from "react";
+import TopCalendar from "@/components/top_calendar/TopCalendar";
+import ScoreCircleBarWrapper from "@/components/summary_score_circle_bar/ScoreCircleBarWrapper";
+import BarGraph from "@/components/global/BarGraph";
+
 export const metadata = {
     title: "Workout Summary",
 };
 
-export default function WorkoutSummary() {
+const WorkoutSummary: React.FC = () => {
+    const score = 80; // Replace with logic to get the score
+    const percent = 80; // Replace with logic to get the percentage
+    const monthlyTotalCalories = 7120; // Replace with logic to get monthly total calories
+    const weeklyTotalCalories = 1550; // Replace with logic to get weekly total calories
+    const dailyTotalCalories = 255; // Replace with logic to get daily total calories
+    const maxCalories = 10000; // Define max calories
+
     return (
         <div>
-            <h1 className="text-2xl font-bold p-2 m-2">
-                This is My Body Buddy Workout Summary page
-            </h1>
-            <div>Hello</div>
+            <TopCalendar />
+            <div className="text-center">
+                <ScoreCircleBarWrapper score={score} percent={percent} />
+                <div className="text-xl font-semibold p-4">Calorie Burned</div>
+                <BarGraph label="Monthly total" value={monthlyTotalCalories} maxValue={maxCalories} />
+                <BarGraph label="Weekly total" value={weeklyTotalCalories} maxValue={maxCalories} />
+                <BarGraph label="Daily total" value={dailyTotalCalories} maxValue={maxCalories} />
+            </div>
         </div>
     );
-}
+};
+
+export default WorkoutSummary;
