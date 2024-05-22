3ab51130cf20a2927f9cb3a21d564bd6134a13e6
Tatsunori Marumo <tmarumo@my.bcit.ca>
2024-05-21 20:08:05 -0700

    Combine Meal model and MealItem model

--- models/Meal.ts
+++ models/Meal.ts
@@ -1,12 +1,18 @@
 import { Schema, model, models } from 'mongoose';
 
+const MealItemSchema = new Schema({
+  name: { type: String, required: true },
+  amount: { type: String, required: true },
+  calories: { type: Number, required: true },
+});
+
 const MealSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   date: { type: Date, required: true },
-  breakfast: [{ type: Schema.Types.ObjectId, ref: 'MealItem' }],
-  lunch: [{ type: Schema.Types.ObjectId, ref: 'MealItem' }],
-  dinner: [{ type: Schema.Types.ObjectId, ref: 'MealItem' }],
-  snacks: [{ type: Schema.Types.ObjectId, ref: 'MealItem' }]
+  breakfast: [MealItemSchema],
+  lunch: [MealItemSchema],
+  dinner: [MealItemSchema],
+  snacks: [MealItemSchema]
 });
 
 const Meal = models.Meal || model('Meal', MealSchema);
