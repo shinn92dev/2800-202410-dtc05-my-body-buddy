a67ada211b4cbcd648ceb5800538e2c59960e50a
Momona-Wada <momona.9410@gmail.com>
2024-05-23 03:12:23 +0900

    Resolve merge conflict

---
+++ components/summary_score_circle_bar/ScoreCircleBarWrapper.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import CircleBar from "../global/CircleBar";
+
+type ScoreCircleBarWrapperProps = {
+    score: number;
+    percent: number;
+};
+
+const ScoreCircleBarWrapper: React.FC<ScoreCircleBarWrapperProps> = ({ score, percent }) => {
+    return (
+        <div className="flex flex-col items-center">
+            <span className="text-2xl font-semibold p-4">Score</span>
+            <CircleBar title={score.toString()} percent={percent} />
+        </div>
+    )
+};
+
+export default ScoreCircleBarWrapper;
