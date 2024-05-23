afed383b63205717c8ff812b2bb4c8f24b70f376
Tatsunori Marumo <tmarumo@my.bcit.ca>
2024-05-22 17:51:41 -0700

    Change Meal Model

--- models/Meal.ts
+++ models/Meal.ts
@@ -2,7 +2,8 @@ import { Schema, model, models } from 'mongoose';
 
 const MealItemSchema = new Schema({
   name: { type: String, required: true },
-  amount: { type: String, required: true },
+  quantity: { type: Number, min: 0.01 ,required: false },
+  unit: { type: String, required: false },
   calories: { type: Number, required: true },
 });
 
