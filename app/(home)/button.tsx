b7c58795b936c8b65d1d71fec2dd8bdf96f0b609
Momona-Wada <momona.9410@gmail.com>
2024-05-23 12:24:50 +0900

    🎨style(home): change button colours

--- app/(home)/button.tsx
+++ app/(home)/button.tsx
@@ -4,7 +4,7 @@ const Button = () => {
     return (
         <div className="flex justify-center space-x-4 p-8">
             <a href="/signup">
-                <button className="bg-orange-500 text-white font-bold py-2 px-4 rounded">
+                <button className="bg-orange text-white font-bold py-2 px-4 rounded">
                     Sign Up
                 </button>
             </a>
