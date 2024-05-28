f2ca5a2274cb72ce32e0472dfea96ac986c1ff1b
Tatsunori Marumo <tmarumo@my.bcit.ca>
2024-05-27 18:26:17 -0700

    Change import path

--- app/(summary)/summary/diet/page.tsx
+++ app/(summary)/summary/diet/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import TopCalendar from "@/components/top_calendar/TopCalendar";
+import TopCalendar from "@/components/global/TopCalendar";
 import ScoreCircleBarWrapper from "@/components/summary_score_circle_bar/ScoreCircleBarWrapper";
 import BarGraph from "@/components/global/BarGraph";
 import WorkoutDietLink from "@/components/workout_diet_link/WorkoutDietLink";
