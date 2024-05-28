4d2c1abf5892a532e0329519d1d73e7b25fa60b1
Momona-Wada <momona.9410@gmail.com>
2024-05-28 15:00:24 +0900

    🎨style(home):Improve landing page UI to make it more simple

--- app/(home)/button.tsx
+++ app/(home)/button.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Button = () => {
     return (
-        <div className="flex justify-center space-x-4 p-8">
-            <a href="/signup">
-                <button className="bg-orange text-white font-bold py-2 px-4 rounded">
+        <div className="flex justify-center flex-col p-8">
+            <a href="/signup" className="flex-1">
+                <button className="w-full min-w-[150px] bg-logo-pumpkin hover:bg-orange-600 text-white font-bold py-3 px-6 mb-4 rounded-full shadow-md transition duration-300">
                     Sign Up
                 </button>
             </a>
-            <a href="/login">
-                <button className="bg-yellow-500 text-white font-bold py-2 px-4 rounded">
+            <a href="/login" className="flex-1">
+                <button className="w-full min-w-[150px] bg-dark-blue hover:bg-blue-600 text-white font-bold py-3 px-6 mb-4 rounded-full shadow-md transition duration-300">
                     Log In
                 </button>
             </a>
