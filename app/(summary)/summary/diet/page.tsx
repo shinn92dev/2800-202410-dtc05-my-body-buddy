56f675c17937f8343c875448148ce7ce813596e0
Momona-Wada <momona.9410@gmail.com>
2024-05-28 11:17:27 +0900

    🎨style: improve style for summary diet page

--- app/(summary)/summary/diet/page.tsx
+++ app/(summary)/summary/diet/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import TopCalendar from "@/components/top_calendar/TopCalendar";
-import ScoreCircleBarWrapper from "@/components/summary_score_circle_bar/ScoreCircleBarWrapper";
+// import ScoreCircleBarWrapper from "@/components/summary_score_circle_bar/ScoreCircleBarWrapper";
 import BarGraph from "@/components/global/BarGraph";
 import WorkoutDietLink from "@/components/workout_diet_link/WorkoutDietLink";
 import AverageCalorieBanner from "@/components/global/AverageCalorieBanner";
@@ -158,11 +158,7 @@ export default function DietSummary() {
                 dietTextColor="text-black"
             />
             <div className="text-center">
-                <ScoreCircleBarWrapper score={80} percent={80} />
-                <AverageCalorieBanner
-                    title="Average Calories intake from"
-                    range={weekRange}
-                />
+                <AverageCalorieBanner title="Average Calories intake from" range={weekRange} />
                 <BarGraph
                     label="Average daily"
                     value={averageDailyCalories}
--- components/global/AverageCalorieBanner.tsx
+++ components/global/AverageCalorieBanner.tsx
@@ -1,3 +1,23 @@
+// import React from "react";
+
+// type AverageCalorieBannerProps = {
+//     title: string;
+//     range: string;
+// }
+
+// const AverageCalorieBanner: React.FC<AverageCalorieBannerProps> = ({ title, range }) => {
+//     return (
+//         <div className="text-lg text-center font-semibold p-4 m-4 bg-beige rounded shadow-md">
+//             {title}
+//             <br />{range}
+//         </div>
+//     );
+// };
+
+// export default AverageCalorieBanner;
+
+
+// use this when we don't have time to implement score calculation
 import React from "react";
 
 type AverageCalorieBannerProps = {
@@ -7,9 +27,13 @@ type AverageCalorieBannerProps = {
 
 const AverageCalorieBanner: React.FC<AverageCalorieBannerProps> = ({ title, range }) => {
     return (
-        <div className="text-lg text-center font-semibold p-4 m-4 bg-beige rounded shadow-md">
-            {title}
-            <br />{range}
+        <div className="flex flex-col items-center justify-center">
+            <img src="/person_only_transparent.png" alt="Logo" className="w-60 h-60 mr-20 mt-[-60px]" />
+            <div className="relative bg-beige text-black text-center font-semibold p-4 rounded-lg shadow-md mt-[-60px] mb-8">
+                {title}
+                <br />{range}
+                <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-b-[10px] border-b-beige border-r-[10px] border-r-transparent border-l-[10px] border-l-transparent "></div>
+            </div>
         </div>
     );
 };
