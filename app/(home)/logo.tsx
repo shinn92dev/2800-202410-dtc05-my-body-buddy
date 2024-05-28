4e83eb4798743a73e87aa6dc387db369dbc2f324
Momona-Wada <momona.9410@gmail.com>
2024-05-28 15:00:24 +0900

    🎨style(home):Improve landing page UI to make it more simple

--- app/(home)/logo.tsx
+++ app/(home)/logo.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Logo = () => {
     return (
-        <div className="flex flex-col items-center justify-center py-10">
+        <div className="flex flex-col items-center justify-center py-4 m-4">
             <img src="/my_body_buddy_logo.png" alt="My Body Buddy Logo" 
-            className="w-32 h-32 rounded"/>
+            className="w-40 h-40 rounded shadow-lg"/>
         </div>
     );
 };
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,4 @@
 import { connectMongoDB } from "@/config/db";
-import Introduction from "./introduction";
 import Logo from "./logo";
 import Encourage from "./encourage";
 import Button from "./button";
@@ -9,22 +8,10 @@ export const metadata = {
 };
 
 export default async function Home() {
-    // console.log(typeof user);
     return (
-        <div>
-            <div
-                className="bg-cover bg-center bg-no-repeat min-w-full flex flex-col items-center justify-center"
-                style={{
-                    backgroundImage:
-                        "url('/healthy_people_temporal_image.png')",
-                    backgroundSize: "cover", // fit the image to the container
-                    color: "black",
-                }}
-            >
-                <Introduction />
-            </div>
-            <Logo />
+        <div className="flex flex-col items-center justify-center">
             <Encourage />
+            <Logo />
             <Button />
         </div>
     );
