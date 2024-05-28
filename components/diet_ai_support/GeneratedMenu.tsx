1c7beb73046b2177e333241a36674e5e4f60c8da
yoshidont-mind <tyoshida6@my.bcit.ca>
2024-05-28 17:11:05 +0900

    Fix bug: size of support-ai-logo shrinks

--- components/diet_ai_support/GeneratedMenu.tsx
+++ components/diet_ai_support/GeneratedMenu.tsx
@@ -23,7 +23,7 @@ const GeneratedMenu: React.FC<GeneratedMenuProps> = ({ generatedMenu }) => {
             <div className="flex flex-col items-start p-2">
                 <div className="flex items-start">
                     <div className="mr-2">
-                        <div style={{width: '75px', height: '75px', backgroundColor: 'transparent'}}></div>
+                        <div style={{width: '50px', height: '50px', backgroundColor: 'transparent'}}></div>
                     </div>
                     <div className="relative speech-bubble-ai bg-beige p-4 rounded-lg">
                         <ReactMarkdown>{generatedMenu}</ReactMarkdown>
