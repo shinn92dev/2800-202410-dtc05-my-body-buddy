a2354dfd0825bc61d3b41d2438f735db4c083689
Tatsunori Marumo <tmarumo@my.bcit.ca>
2024-05-29 16:43:00 -0700

    Create authentication for profile page

--- app/(home)/logo.tsx
+++ app/(home)/logo.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Image from "next/image";
 
 const Logo = () => {
     return (
         <div className="flex flex-col items-center justify-center py-4 m-4">
-            <img src="/my_body_buddy_logo.png" alt="My Body Buddy Logo" 
-            className="w-40 h-40 rounded shadow-lg"/>
+            <Image src="/my_body_buddy_logo.png" alt="My Body Buddy Logo" 
+            className="w-40 h-40 rounded shadow-lg"
+            width={80}
+            height={80}/>
         </div>
     );
 };
--- app/(profile)/profile/page.tsx
+++ app/(profile)/profile/page.tsx
@@ -1,9 +1,11 @@
 import ProfileWrapper from "@/components/profile/ProfileWrapper";
+import authenticateUser from "@/app/_helper/authenticateUser";
 
 export const metadata = {
   title: "User Profile",
 };
 
-export default function UserProfilePage() {
+export default async function UserProfilePage() {
+  await authenticateUser();
   return <ProfileWrapper />;
 }
