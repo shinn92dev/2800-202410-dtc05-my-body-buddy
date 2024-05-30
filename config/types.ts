70f1c701d3182e19a38a301cbe78b1de4a7eb2ef
Anthony Shin <shinn92dev@gmail.com>
2024-05-29 21:42:03 -0700

    refactor (types): create programmer defiend type file

---
+++ config/types.ts
@@ -0,0 +1,17 @@
+export interface WorkoutDetail {
+    name: string;
+    calories: number;
+    unit: string;
+    quantity: number;
+    achieved: boolean;
+}
+
+export interface Workout {
+    date: Date;
+    workoutDetail: WorkoutDetail[];
+}
+
+export interface WorkoutData {
+    userId: string;
+    workouts: Workout[];
+}
