583208a9216deda8f598e5265bec92597ea75b61
Tatsunori Marumo <tmarumo@my.bcit.ca>
2024-05-31 03:56:08 -0700

    Fetch target data in workout summary

--- components/global/BarGraph.tsx
+++ components/global/BarGraph.tsx
@@ -1,15 +1,16 @@
-import { max } from 'date-fns';
 import React from 'react';
 
 type BarGraphProps = {
     label: string;
     value: number;
     maxValue: number;
+    targetValue?: number;
 };
 
-const BarGraph: React.FC<BarGraphProps> = ({ label, value, maxValue }) => {
+const BarGraph: React.FC<BarGraphProps> = ({ label, value, maxValue, targetValue }) => {
     const percentage = Math.min((value / maxValue) * 100, 100);
-    const barColor = value > maxValue ? "bg-red-500" : "bg-logo-pumpkin";
+    const targetPercentage = targetValue ? Math.min((targetValue / maxValue) * 100, 100) : null;
+    const barColor = value > maxValue * 0.83 ? "bg-logo-pumpkin" : "bg-orange";
 
     return (
         <div className="w-full mb-4 px-8">
@@ -17,11 +18,17 @@ const BarGraph: React.FC<BarGraphProps> = ({ label, value, maxValue }) => {
                 <span className="font-semibold">{label}</span>
                 <span className="font-semibold">{value}kcal</span>
             </div>
-            <div className="w-full bg-gray-300 h-6 rounded">
+            <div className="w-full bg-gray-300 h-6 rounded relative">
                 {value === 0 ? (
                     <div className="bg-gray-300 h-full rounded"></div>
                 ) : (
                     <div className={`${barColor} h-full rounded`} style={{ width: `${percentage}%` }}></div>
+                )}
+                {targetPercentage !== null && (
+                    <div
+                        className="absolute top-0 left-0 h-full border-r-4 border-dark-blue"
+                        style={{ left: `${targetPercentage}%` }}
+                    ></div>
                 )}
             </div>
         </div>
