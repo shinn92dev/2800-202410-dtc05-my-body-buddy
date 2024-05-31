d3d1eee852d026fee59c0cec06d4fda2e62b42e0
Tatsunori Marumo <tmarumo@my.bcit.ca>
2024-05-30 17:46:22 -0700

    Add other for gender

--- components/profile_edit/ProfileEditWrapper.tsx
+++ components/profile_edit/ProfileEditWrapper.tsx
@@ -95,6 +95,7 @@ const ProfileEditWrapper: React.FC = () => {
             <option value="">Select Gender</option>
             <option value="male">Male</option>
             <option value="female">Female</option>
+            <option value="other">Other</option>
           </select>
         </div>
         <div className="mb-4">
