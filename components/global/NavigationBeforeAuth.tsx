cf609941c96280effe061f233fbb7c7896f22014
Anthony Shin <shinn92dev@gmail.com>
2024-05-31 03:49:21 -0700

    feat (middleware): implement middleware logic to redirec unlogged-in user to login page

--- components/global/NavigationBeforeAuth.tsx
+++ components/global/NavigationBeforeAuth.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
 
